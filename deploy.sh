7da26b47f4612fb3de72741423764beec082e131
roy chiu <manshan.chiu@grabtaxi.com>
2022-07-18 15:58:45 +0800

    point to correct kaniko

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ if [ $# -eq 0  ] || [ -z "$1" ] || { [ $1 != "manager" ] && [ $1 != "runner" ]; 
     exit 1
 fi
 
-export ECR_URI=xxxxxxxx.dkr.ecr.ap-southeast-1.amazonaws.com
+export ECR_URI=635353606081.dkr.ecr.ap-southeast-1.amazonaws.com
 export ECR_REPO=gitlab-runner
 
 if [ "$1" = "manager" ]; then 
